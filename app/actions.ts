267f5c23b1d919f7fd0b58365d2bdcb14ef84523
Vibhor Agarwal <agarwalvibhor930@gmail.com>
2024-06-12 18:47:29 +0530

    Added code for storing images in supabase and form action to submit the data

--- app/actions.ts
+++ app/actions.ts
@@ -2,6 +2,7 @@
 
 import prisma from "./lib/db";
 import { redirect } from "next/navigation";
+import { supabase } from "./lib/supabase";
 
 export async function createAirbnbHome({ userId }: { userId: string }) {
   const data = await prisma.home.findFirst({
@@ -46,5 +47,38 @@ export async function createCategoryPage(formData: FormData) {
 }
 
 export async function CreateDescription(formData: FormData) {
-  
+  const title = formData.get("title") as string;
+  const description = formData.get("description") as string;
+  const price = formData.get("price");
+  const imageFile = formData.get("image") as File;
+
+  const guestNumber = formData.get("guest") as string;
+  const roomNumber = formData.get("room") as string;
+  const bathroomNumber = formData.get("bathroom") as string;
+  const homeId = formData.get("homeId") as string;
+
+  const { data: imageData } = await supabase.storage
+    .from("images")
+    .upload(`${imageFile.name}-${new Date()}`, imageFile, {
+      cacheControl: "2592000",
+      contentType: "image/png/jpg/jpeg/webp",
+    });
+
+  const data = await prisma.home.update({
+    where: {
+      id: homeId,
+    },
+    data: {
+      title: title,
+      description: description,
+      price: Number(price),
+      bedrooms: roomNumber,
+      bathrooms: bathroomNumber,
+      guests: guestNumber,
+      photo: imageData?.path,
+      addedDescription: true,
+    },
+  });
+
+  return redirect(`/create/${homeId}/address`);
 }
