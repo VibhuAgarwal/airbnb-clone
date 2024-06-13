3b85e6c376df31f785dfffbeef9c32197624a735
Vibhor Agarwal <agarwalvibhor930@gmail.com>
2024-06-13 15:04:38 +0530

    Added map component in location component

---
+++ app/components/Map.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import { MapContainer, TileLayer, Marker } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+
+export default function Map() {
+  return (
+    <MapContainer
+      scrollWheelZoom={false}
+      className="h-[50vh] rounded-lg z-0"
+      center={[52.505, -0.09]}
+      zoom={13}
+    >
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+    </MapContainer>
+  );
+}
--- app/create/[id]/address/page.tsx
+++ app/create/[id]/address/page.tsx
@@ -1,3 +1,55 @@
-export default function Address() {
-  return <h1>Address Route</h1>;
+import { CreationBottomBar } from "@/app/components/CreationBottomBar";
+import { useCountries } from "@/app/lib/getCountries";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { Skeleton } from "@/components/ui/skeleton";
+import dynamic from "next/dynamic";
+
+export default function AddressRoute() {
+  const { getAllCountries } = useCountries();
+
+  const LazyMap = dynamic(() => import("@/app/components/Map"), {
+    ssr: false,
+    loading: () => <Skeleton className="h-[50vh] w-full" />,
+  });
+  return (
+    <>
+      <div className="w-3/5 mx-auto">
+        <h2 className="text-3xl font-semibold tracking-tight transition-colors mb-10">
+          Where is <span className="text-[#FF5A5F]">your home</span> located
+        </h2>
+      </div>
+      <form>
+        <div className="w-3/5 mx-auto mb-36">
+          <div className="mb-5">
+            <Select required>
+              <SelectTrigger className="w-full">
+                <SelectValue placeholder="Select a country" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectGroup>
+                  <SelectLabel>Countries</SelectLabel>
+                  {getAllCountries().map((item) => (
+                    <SelectItem key={item.value} value={item.value}>
+                      {item.flag} {item.label} / {item.region}
+                    </SelectItem>
+                  ))}
+                </SelectGroup>
+              </SelectContent>
+            </Select>
+          </div>
+
+          <LazyMap />
+        </div>
+        <CreationBottomBar />
+      </form>
+    </>
+  );
 }
